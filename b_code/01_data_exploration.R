ff00d9c49d004eb349578fe7803c22e807d9e59b
Purity K <puritykoima2020@gmail.com>
2026-06-10 04:40:25 +0300

    Create 01_data_exploration.R for dataset exploration
    Add initial script for data exploration of Nutrition dataset.

---
+++ b_code/01_data_exploration.R
@@ -0,0 +1,7 @@
+# =====================================================
+# Script: 01_data_exploration.R
+# Project: Global Anaemia Prevalence Analysis
+# Purpose: Explore dataset structure and variables
+# =====================================================
+
+View(Nutrition)
